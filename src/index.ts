86d0c6de2c56af3b66b818f7df62db41d3c97cb7
mohmamad <mohamad.isa.moghrabi@gmail.com>
2026-02-19 00:02:30 +0200

    create feed table and add addfeed command

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,44 @@
 // Entry point of the application.
 // It processes command-line arguments, sets up the command registry, and executes the appropriate command based on user input.
 
+import { handlerAggregator } from "./aggregatorHandler";
 import { CommandsRegistry, registerCommand, runCommand } from "./commands";
-import { handlerGetUsers } from "./getUsersHandler";
-import { handlerLogin } from "./loginHandler";
-import { handlerRigster } from "./registerHandler";
-import { handlerReset } from "./resetHandler";
+import { handlerGetUsers } from "./userHandler/getUsersHandler";
+import { handlerLogin } from "./userHandler/loginHandler";
+import { handlerRigster } from "./userHandler/registerHandler";
+import { handlerReset } from "./userHandler/resetHandler";
+import { handlerAddFeed } from "./feedHandler/addFeedHandler";
+import { argv } from "process";
+import { printAllFeeds } from "./feedHandler/printFeedHandler";
+import { handlerFollow, handlerFollowing } from "./feedHandler/followHandler";
 
 // Main function to process command-line arguments and execute commands
 async function main() {
-  const commandargs = ["login", "register"];
-  if (process.argv.length === 2) {
-    console.error("Not enough arguments were provided.");
-    process.exit(1);
-  } else if (
-    process.argv.length === 3 &&
-    commandargs.includes(process.argv[2])
-  ) {
-    console.error("A username is required");
-    process.exit(1);
-  }
   const commandRegistry: CommandsRegistry = {};
   registerCommand(commandRegistry, "login", handlerLogin);
   registerCommand(commandRegistry, "register", handlerRigster);
   registerCommand(commandRegistry, "reset", handlerReset);
   registerCommand(commandRegistry, "users", handlerGetUsers);
-  const command = process.argv.slice(2);
+  registerCommand(commandRegistry, "agg", handlerAggregator);
+  registerCommand(commandRegistry, "addfeed", handlerAddFeed);
+  registerCommand(commandRegistry, "feeds", printAllFeeds);
+  registerCommand(commandRegistry, "follow", handlerFollow);
+  registerCommand(commandRegistry, "following", handlerFollowing);
+
+  const commandargs = ["login", "register"];
+  if (process.argv.length === 2) {
+    console.error("Not enough arguments were provided.");
+    process.exit(1);
+  } else if (process.argv.length === 3 && commandargs.includes(argv[2])) {
+    console.error("A username is required");
+    process.exit(1);
+  } else if (process.argv.length < 5 && argv[2] === "addfeed") {
+    console.error("Invalid command or insufficient arguments.");
+    process.exit(1);
+  }
+  const command = argv.slice(2);
   try {
-    await runCommand(commandRegistry, command[0], command[1]);
+    await runCommand(commandRegistry, command[0], ...command.slice(1));
   } catch (error) {
     if (error instanceof Error) {
       console.error(error.message);
---
+++ src/lib/db/queries/feeds.ts
@@ -0,0 +1,33 @@
+// This file defines the database query functions related to feeds, including adding a new feed to the database.
+import { eq } from "drizzle-orm";
+import { db } from "..";
+import { feeds, users } from "../schema";
+import { readConfig } from "../../../config";
+
+// Function to add a new feed to the database, associated with the currently logged-in user.
+export async function addFeed(name: string, URL: string) {
+  const userName = readConfig().currentUserName;
+  const userId = await db
+    .select()
+    .from(users)
+    .where(eq(users.name, userName))
+    .then((res) => res[0].id);
+  if (!userId) {
+    throw new Error("User not found");
+  }
+  const insertedFeed = await db
+    .insert(feeds)
+    .values({ url: URL, name: name, userId: userId })
+    .returning();
+  return insertedFeed[0];
+}
+
+// Function to retrieve all feeds from the database.
+export async function getFeeds() {
+  return await db.select().from(feeds);
+}
+
+export async function getFeedByUrl(url: string) {
+  const [result] = await db.select().from(feeds).where(eq(feeds.url, url));
+  return result;
+}
